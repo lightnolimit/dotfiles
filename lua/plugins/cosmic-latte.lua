953f48e2418b26379e7a4931adfb9f8a1da34c0a
Lite <lightwithoutlimit@proton.me>
2025-07-14 13:55:53 -0400

    feat: update cosmic-latte to use external git repository

--- lua/plugins/cosmic-latte.lua
+++ lua/plugins/cosmic-latte.lua
@@ -1,43 +1,34 @@
 return {
   {
-    "cosmic-latte.nvim",
-    dir = vim.fn.stdpath("config") .. "/cosmic-latte.nvim",
-    name = "cosmic-latte",
+    "lightnolimit/cosmic-latte-theme-nvim",
     priority = 1000,
     lazy = false,
     config = function()
       vim.opt.termguicolors = true
-      -- Default to dim theme, use :colorscheme cosmic-latte for light version
-      vim.cmd("colorscheme cosmic-latte-dim")
-      
-      -- Add commands to switch between themes
-      vim.api.nvim_create_user_command("CosmicLatteDim", function()
-        vim.cmd("colorscheme cosmic-latte-dim")
-      end, { desc = "Switch to Cosmic Latte Dim theme" })
-      
-      vim.api.nvim_create_user_command("CosmicLatteLight", function()
-        vim.cmd("colorscheme cosmic-latte")
-      end, { desc = "Switch to Cosmic Latte Light theme" })
+      -- Set default variant to dim (Cosmic Mocha Latte)
+      vim.g.cosmic_latte_variant = "dim"
+      -- Load the colorscheme
+      vim.cmd("colorscheme cosmic-latte")
       
       -- Quick hotkeys for theme switching
       vim.keymap.set("n", "<leader>tl", function()
-        vim.cmd("colorscheme cosmic-latte")
+        vim.cmd("CosmicLatteLight")
         vim.notify("Switched to Light theme", vim.log.levels.INFO)
       end, { desc = "Switch to light theme" })
       
       vim.keymap.set("n", "<leader>td", function()
-        vim.cmd("colorscheme cosmic-latte-dim") 
-        vim.notify("Switched to Dim theme", vim.log.levels.INFO)
+        vim.cmd("CosmicLatteDim") 
+        vim.notify("Switched to Dim theme (Cosmic Mocha Latte)", vim.log.levels.INFO)
       end, { desc = "Switch to dim theme" })
       
       vim.keymap.set("n", "<leader>tc", function()
         -- Toggle between light and dim
         if vim.g.colors_name == "cosmic-latte-dim" then
-          vim.cmd("colorscheme cosmic-latte")
+          vim.cmd("CosmicLatteLight")
           vim.notify("Switched to Light theme", vim.log.levels.INFO)
         else
-          vim.cmd("colorscheme cosmic-latte-dim")
-          vim.notify("Switched to Dim theme", vim.log.levels.INFO)
+          vim.cmd("CosmicLatteDim")
+          vim.notify("Switched to Dim theme (Cosmic Mocha Latte)", vim.log.levels.INFO)
         end
       end, { desc = "Toggle between light/dim themes" })
     end,
